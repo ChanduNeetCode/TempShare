2ddb9f2983599a01aaa4e970257e5c2719a209ad
ChanduNeetCode <chandudp01@gmail.com>
2025-09-16 11:30:11 +0530

    Update Tempsharing.java

--- Tempsharing.java
+++ Tempsharing.java
@@ -1,37 +1,83 @@
-import React, { createContext, useState, ReactNode, useCallback, Dispatch, SetStateAction } from 'react';
-import { STRINGS } from '../constant/strings';
+class Solution {
 
-interface reactivationDetailsProps {
-  isEnable : boolean;
-  title :string;
-  subTitle : string;
-  buttonTitle : string
+    public int findGCD(int a, int b){
+
+        while(b != 0){
+
+            int temp = b;
+
+            b = a % b;
+
+            a = temp;
+
+        }
+
+        return a;
+
+    }
+
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+
+        int n = nums.length;
+
+        List<Integer> ans = new ArrayList<>();
+
+        if(n == 1){
+
+            return new ArrayList<>(Arrays.asList(nums[0]));
+
+        }
+
+        Stack<Integer> st = new Stack<>();
+
+        st.push(nums[0]);
+
+        st.push(nums[1]);
+
+        int i = 2;
+
+        while(i < n){
+
+            if(st.size() < 2){
+
+                st.push(nums[i++]);
+
+            }
+
+            int a = st.pop();
+
+            int b = st.pop();
+
+            int gcd = findGCD(a, b);
+
+            if(gcd > 1){
+
+                st.push((a * b)/ gcd);
+
+            }
+
+            else{
+
+                st.push(a);
+
+                st.push(b);
+
+                st.push(nums[i]);
+
+                i++;
+
+            }
+
+        }
+
+        while(st.size() != 0){
+
+            ans.add(st.pop());
+
+        }
+
+        return ans;
+
+    }
+
 }
-
-interface OffersStatusNavViewHeightContextProps {
-  reactivationDetails : reactivationDetailsProps;
-  setReactivationDetails : (val : reactivationDetailsProps) => void;
-}
-
-
-
-export const OffersStatusNavViewHeightContext =
-  createContext<OffersStatusNavViewHeightContextProps>({
-    reactivationDetails :{isEnable : false, title : String(STRINGS.str_reactivationRequired), subTitle : String(STRINGS.str_reactivateMessage), buttonTitle : String(STRINGS.str_reactivate)},
-    setReactivationDetails :()=>{},
-  });
-
-export const OffersStatusNavViewHeightProvider: React.FC<{
-  children: ReactNode;
-}> = ({ children }) => {
-  const [reactivationDetails, setReactivationDetails] = useState({isEnable: false, title :STRINGS.str_reactivationRequired, subTitle : STRINGS.str_reactivateMessage, buttonTitle : STRINGS.str_reactivate});
-  return (
-    <OffersStatusNavViewHeightContext.Provider
-      value={{
-        reactivationDetails,
-        setReactivationDetails,
-      }}>
-      {children}
-    </OffersStatusNavViewHeightContext.Provider>
-  );
-};
