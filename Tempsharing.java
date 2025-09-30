c8cc6710774379e0f66a8d59966dab5930be86ec
ChanduNeetCode <chandudp01@gmail.com>
2025-09-30 10:45:24 +0530

    Update Tempsharing.java

--- Tempsharing.java
+++ Tempsharing.java
@@ -1,83 +1,21 @@
 class Solution {
+    public int triangularSum(int[] nums) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int num :nums){
+            q.add(num);
+        }
+        q.add(-1);
+        while(q.size() != 2){
+            int a = q.poll();
+            int b = q.peek();
+            if(a == -1) continue;
+            if(b == -1){
+                q.add(-1);
+                continue;
+            }
 
-    public int findGCD(int a, int b){
-
-        while(b != 0){
-
-            int temp = b;
-
-            b = a % b;
-
-            a = temp;
-
-        }
-
-        return a;
-
-    }
-
-    public List<Integer> replaceNonCoprimes(int[] nums) {
-
-        int n = nums.length;
-
-        List<Integer> ans = new ArrayList<>();
-
-        if(n == 1){
-
-            return new ArrayList<>(Arrays.asList(nums[0]));
-
-        }
-
-        Stack<Integer> st = new Stack<>();
-
-        st.push(nums[0]);
-
-        st.push(nums[1]);
-
-        int i = 2;
-
-        while(i < n){
-
-            if(st.size() < 2){
-
-                st.push(nums[i++]);
-
-            }
-
-            int a = st.pop();
-
-            int b = st.pop();
-
-            int gcd = findGCD(a, b);
-
-            if(gcd > 1){
-
-                st.push((a * b)/ gcd);
-
-            }
-
-            else{
-
-                st.push(a);
-
-                st.push(b);
-
-                st.push(nums[i]);
-
-                i++;
-
-            }
-
-        }
-
-        while(st.size() != 0){
-
-            ans.add(st.pop());
-
-        }
-
-        return ans;
-
-    }
-
+            q.add((a + b) % 10);
+        }
+        return q.peek();
+    }
 }
