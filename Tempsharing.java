ff82415d94512b7be66c6f90ad85169a07e32ce9
ChanduNeetCode <chandudp01@gmail.com>
2025-10-07 12:51:41 +0530

    Update Tempsharing.java

--- Tempsharing.java
+++ Tempsharing.java
@@ -1,13 +1,11 @@
-
 class Solution {
     public static boolean checkSubsequenceSum(int N, int[] arr, int K) {
         // code here
-        Arrays.sort(arr);
         int l = 0, r = 0;
         int sum = 0;
         while(r < N){
             sum += arr[r];
-            while(sum > K){
+            while(sum > K && l <=r){
                 sum -= arr[l];
                 l++;
             }
