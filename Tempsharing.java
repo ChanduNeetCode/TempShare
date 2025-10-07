f6719d5ccefe936786de71328f4eb43b37456fc1
ChanduNeetCode <chandudp01@gmail.com>
2025-10-07 12:59:10 +0530

    Update Tempsharing.java

--- Tempsharing.java
+++ Tempsharing.java
@@ -1,17 +1,16 @@
+// User function Template for Java
+
 class Solution {
+    public static boolean helper(int i, int sum, int[] arr, int n, int k, boolean[] dp){
+        if(sum == k) return dp[i] = true;
+        if(i == n) return false;
+        
+        return dp[i] = helper(i +1, sum + arr[i], arr, n, k,dp) || helper(i + 1,sum, arr, n, k, dp);
+    }
     public static boolean checkSubsequenceSum(int N, int[] arr, int K) {
         // code here
-        int l = 0, r = 0;
-        int sum = 0;
-        while(r < N){
-            sum += arr[r];
-            while(sum > K && l <=r){
-                sum -= arr[l];
-                l++;
-            }
-            if(sum == K) return true;
-            r++;
-        }
-        return false;
+        boolean[] dp = new boolean[N + 1];
+        helper(0 ,0 ,arr, N, K, dp);
+        return dp[N];
     }
 }
