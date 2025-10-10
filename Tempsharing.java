d3450632c784e9b98630f27fd44b45cc5a6d821d
ChanduNeetCode <chandudp01@gmail.com>
2025-10-10 12:02:17 +0530

    Update Tempsharing.java

--- Tempsharing.java
+++ Tempsharing.java
@@ -1,25 +1,51 @@
-class Solution {
-    public void helper(int num,int sum,List<Integer> carry ,List<List<Integer>> res , int k, int n){
-        if(sum > n) return;
-        if(carry.size() == k){
-            if(sum == n){
-                res.add(new ArrayList<>(carry));
-            }
+class MinHeap {
+    int[] arr;
+    int cap;
+    int size;
+    
+    MinHeap(int capacity){
+        cap = capacity;
+        size = 0;
+        arr = new int[capacity];
+    }
+    
+    int parent (int i) { return (i-1)/2; }
+    int left (int i) { return 2 * i + 1; }
+    int right (int i) { return 2 * i + 2; }
+    
+    public int extractMin(){
+        int min = arr[0];
+        deleteKey(0);
+        return min;
+    }
+    
+    public void swap(int i, int j){
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    
+    public void upHeap(int i){
+        int index = i;
+        while(i >= 0 && arr[parent(i)] > arr[i]){
+            swap(i, parent(i));
+            i = parent(i);
+        }
+    }
+    
+    public void insertKey(int k){
+        arr[size] = k;
+        upHeap(size);
+        size++;
+    }
+    
+    public void deleteKey(int i){
+        if(size-1 < 0){
+            size = 0;
             return;
         }
-
-        //take
-        carry.add(num);
-        helper(num + 1, sum + num, carry, res, k, n);
-        carry.remove(carry.size() - 1);
-
-        
-        helper(num + 1, sum, carry, res, k, n);
-    }
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> carry = new ArrayList<>();
-        helper(1, 0, carry, res, k, n);
-        return res;
+        swap(i, size - 1);
+        size--;
+        upHeap(i);
     }
 }
